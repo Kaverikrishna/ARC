e1c74daafd6eab3e1623cc0810b706004f5abe35
krishna <krishnasonepluse@gmail.com>
2023-06-26 09:46:54 +0530

    routing added to Application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import { AppComponent } from './app.component';
+import { UserComponent } from './feature/user/user.component';
+import { HomeComponent } from './feature/home/home.component';
+import { AdminComponent } from './feature/admin/admin.component';
+import { NotFoundComponentComponent } from './feature/not-found-component/not-found-component.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '' , component : HomeComponent },
+  { path:'user' , component : UserComponent },
+  { path: 'home', component : HomeComponent },
+  { path: 'admin', component: AdminComponent  },
+  { path: '**', component: NotFoundComponentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './feature/user/user.component';
+import { HomeComponent } from './feature/home/home.component';
+import { AdminComponent } from './feature/admin/admin.component';
+import { NotFoundComponentComponent } from './feature/not-found-component/not-found-component.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    HomeComponent,
+    AdminComponent,
+    NotFoundComponentComponent
   ],
   imports: [
     BrowserModule,
